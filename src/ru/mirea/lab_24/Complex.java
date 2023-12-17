6f541ad54ec11491591c89d57176947abb79a7ea
unknown <chrnv1@mail.ru>
2023-12-17 21:04:55 +0300

    lab_24

---
+++ src/ru/mirea/lab_24/Complex.java
@@ -0,0 +1,9 @@
+package ru.mirea.lab_24;
+
+public interface Complex {
+    double getReal();
+
+    double getImage();
+
+    String toString();
+}
---
+++ src/ru/mirea/lab_24/ComplexAbstractFactory.java
@@ -0,0 +1,6 @@
+package ru.mirea.lab_24;
+
+public interface ComplexAbstractFactory {
+    Complex createComplex(int real, int image);
+
+}
